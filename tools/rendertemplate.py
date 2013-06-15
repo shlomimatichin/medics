ab6dc6b8d766089a73ca5e1c381e7a57d8777d96
Shlomi Matichin <shlomi@stratoscale.com>
2013-06-15 22:16:48 +0300

    new document: dinei taavura

--- tools/rendertemplate.py
+++ tools/rendertemplate.py
@@ -1,9 +1,10 @@
-import django.template.loader
 import os
-import sys
-
 TEMPLATE_DIRS = [ 'templates', '.' ]
 os.environ[ 'DJANGO_SETTINGS_MODULE' ] = '__main__'
+SECRET_KEY = "ABAB"
+
+import django.template.loader
+import sys
 
 content = django.template.loader.render_to_string( sys.argv[ 1 ], {} )
 open( sys.argv[ 2 ], "wb" ).write( content.encode( 'utf-8' ) )
